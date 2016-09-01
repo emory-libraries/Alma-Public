df0358b52a5e2dc5394ee4b53c82f6cf58b93320
Alex <alexander.g.cooper@gmail.com>
2016-09-01 17:22:48 -0400

    Create delete_oclc_holdings.sh

---
+++ oclc_holdings_apis/delete_oclc_holdings.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+alma_environ="/alma/config/environ"
+. ${alma_environ}  # include environment file
+# export all of the environ variables to my children
+for env_var in $(cat ${alma_environ} | awk -F'=' '{print $1}')
+do
+  export ${env_var}
+done
+
+bin_dir="/alma/bin/"
+config="/alma/config/produce_deleted_records.cfg"
+config2="/alma/config/produce_withdrawn_records.cfg"
+error_log="/tmp/delete_oclc_log.$$"
+error_log2="/tmp/delete_oclc_log2.$$"
+log_parsed="/tmp/delete_oclc_log.txt"
+log_parsed2="/tmp/delete_oclc_log2.txt"
+mail_list="acoope5@emory.edu egrant7@emory.edu"
+
+####unset holdings for deleted titles
+${bin_dir}delete_oclc_holdings_api.py ${config} 2> ${error_log}
+cat ${error_log} | grep exists > ${log_parsed}
+mutt -a ${log_parsed} -s "Delete OCLC Holdings Report" -- ${mail_list} <<EOM
+attached is today's log for deleted.
+EOM
+
+####unset holdings for withdrawn titles
+${bin_dir}delete_oclc_holdings_api.py ${config2} 2> ${error_log2}
+cat ${error_log2} | grep Alma > ${log_parsed2}
+mutt -a ${log_parsed} -s "Withdrawn OCLC Holdings Report" -- ${mail_list} <<EOM
+attached is today's log for withdrawn.
+EOM
+
+exit 0
