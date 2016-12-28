760fd81a778568c5a2074e480e2ae7b18c8f39d0
Alex <alexander.g.cooper@gmail.com>
2016-12-28 16:27:29 -0500

    Update patrons_no_email.sh

--- PatronsWithNoEmails/patrons_no_email.sh
+++ PatronsWithNoEmails/patrons_no_email.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-alma_environ="/alma/config/environ"
-. ${alma_environ}  # include environment file
-# export all of the environ variables to my children
-for env_var in $(cat ${alma_environ} | awk -F'=' '{print $1}')
-do
-  export ${env_var}
-done
-
 todays_date=$(date +"%Y%m%d")
 dow=$(date +%w)
-#mail_list="acoope5@emory.edu"
-mail_list="acoope5@emory.edu,jperlov@emory.edu"
+mail_list="[email]"
 bindir="/alma/bin/"
 script=${bindir}patrons_no_email_api.py
 configdir="/alma/config/"
