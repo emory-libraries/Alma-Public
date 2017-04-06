982078523e82bda6f954c03edb53cfdfe3250683
Alex <alexander.g.cooper@gmail.com>
2017-04-06 15:40:42 -0400

    Cleans out socks and socket

--- DeleteOclcHoldings/get_alma_deleted_holdings.py
+++ DeleteOclcHoldings/get_alma_deleted_holdings.py
@@ -1,4 +1,4 @@
-#!/bin/env python
+#!/bin/python
 # -*- coding: utf-8 -*-
 r"""
    Author: Alex Cooper
@@ -9,8 +9,6 @@ import os
 import sys
 import random
 import re
-import socks
-import socket
 import requests
 import xml.etree.ElementTree as elementTree
 import datetime as dt
@@ -70,8 +68,6 @@ def check_oclc_numbers(id,query):
     oclc_no = oclc_no[1]
     oclc_no = str(oclc_no)
     queryParams = urlencode({ query : oclc_no })
-    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 8080)
-    socket.socket = socks.socksocket
     try:
         request = Request(url + queryParams)
         result = urlopen(request).read()
@@ -113,8 +109,6 @@ def main():
 
   in_string=""
   outcome=1
-  socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 8080)
-  socket.socket = socks.socksocket
   payload={'apikey':apikey,'path':path,'limit':limit}
   sys.stderr.write("analytics path:"+str(path)+"\n")
   try:
@@ -168,9 +162,6 @@ def main():
       work_to_do=True
       outcome=1
       while work_to_do:
-        
-         socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 8080)
-         socket.socket = socks.socksocket
          payload={'apikey':apikey,'token':this_token,'limit':limit}
          try:
             r=requests.get(url,params=payload)
