779ff19235c3191c76b8084da773d2e86d8bb397
Alex <alexander.g.cooper@gmail.com>
2017-12-28 12:26:15 -0500

    Adds alma_rdf.py

---
+++ alma_rdf/alma_rdf.py
@@ -0,0 +1,39 @@
+#!/bin/python
+import sys
+import cgi
+import cgitb; cgitb.enable(display=1, logdir="/tmp")
+import urllib2
+
+def get_rdf(mmsid,inst):
+    url = "https://open-na.hosted.exlibrisgroup.com/alma/" + inst + "/rda/entity/manifestation/" + mmsid + ".rdf"
+    try:
+        socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 8080)
+        socket.socket = socks.socksocket
+    except:
+        sys.stderr.write("could not open socket" + "\n")
+    rdf = urllib2.urlopen(url).read()
+    return rdf,url
+
+def main():
+
+    form = cgi.FieldStorage()
+    target = open("/alma/webserver/docs/alma_rdf.html", 'rU')
+    inst = form.getfirst('institution')
+    mmsid = form.getfirst('mmsid')
+    
+    if len(inst) == 0 or len(mmsid) == 0:
+        print '%s' % "Content-Type: text/html; charset=utf-8"
+        print ""
+        for line in target:
+            print line
+        target.close()
+    else:
+        rdf,url = get_rdf(mmsid,inst)
+        print '%s' % "Content-Type: text/html; charset=utf-8"
+        print ""
+        print "<xmp>"
+        print str(rdf)
+        print "</xmp>"
+
+if __name__=="__main__":
+    sys.exit(main())
